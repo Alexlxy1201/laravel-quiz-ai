34526a9b3d8490a719704e86a85cb159597937be
Alexlxy2001 <alex.lxy.1201@gmail.com>
2025-10-17 22:58:37 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SolveController;
 
 Route::get('/', [SolveController::class, 'index'])->name('solve.index');
-#Route::post('/api/solve', [SolveController::class, 'solve'])->name('solve.api');
+Route::post('/api/solve', [SolveController::class, 'solve'])->name('solve.api');
 Route::post('/api/solve', function () {
     return response()->json(['ok' => true, 'msg' => 'Backend reachable ✅']);
 });
